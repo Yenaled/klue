505aa6576cb209c2fee4331cdfb036a43d3aab23
Yenaled <delaneyk.sullivan@gmail.com>
2023-07-16 00:57:17 -0700

    start reconstruct

--- src/main.cpp
+++ src/main.cpp
@@ -171,6 +171,99 @@ void ParseOptionsDistinguish(int argc, char **argv, ProgramOptions& opt) {
 }
 
 
+void ParseOptionsRefineUnitigs(int argc, char **argv, ProgramOptions& opt) {
+  int verbose_flag = 0;
+  int pipe_flag = 0;
+  int distinguish_all_flag = 0;
+  int distinguish_all_but_one_flag = 0;
+  const char *opt_string = "o:k:m:t:r:p";
+  static struct option long_options[] = {
+    // long args
+    {"verbose", no_argument, &verbose_flag, 1},
+    {"all", no_argument, &distinguish_all_flag, 1},
+    {"all-but-one", no_argument, &distinguish_all_but_one_flag, 1},
+    // short args
+    {"output", required_argument, 0, 'o'},
+    {"pipe", no_argument, &pipe_flag, 'p'},
+    {"kmer-size", required_argument, 0, 'k'},
+    {"min-size", required_argument, 0, 'm'},
+    {"threads", required_argument, 0, 't'},
+    {"range", required_argument, 0, 'r'},
+    {0,0,0,0}
+  };
+  int c;
+  int option_index = 0;
+  while (true) {
+    c = getopt_long(argc,argv,opt_string, long_options, &option_index);
+    
+    if (c == -1) {
+      break;
+    }
+    
+    switch (c) {
+    case 0:
+      break;
+    case 'o': {
+      opt.distinguish_output_fasta = optarg;
+      break;
+    }
+    case 'p': {
+      pipe_flag = 1;
+      break;
+    }
+    case 'k': {
+      stringstream(optarg) >> opt.k;
+      break;
+    }
+    case 'm': {
+      stringstream(optarg) >> opt.g;
+      break;
+    }
+    case 't': {
+      stringstream(optarg) >> opt.threads;
+      break;
+    }
+    case 'r': {
+      std::string range_input_str;
+      stringstream(optarg) >> range_input_str;
+      stringstream ss(range_input_str);
+      std::string range_val;
+      int i = 0;
+      char delimeter = '-';
+      if (range_input_str.find(',') < range_input_str.length()) {
+        delimeter = ','; // If string contains commas, use commas as delimeter
+      }
+      while (std::getline(ss, range_val, delimeter)) { 
+        if (i == 0) opt.distinguish_range_begin = std::atoi(range_val.c_str());
+        if (i == 1) opt.distinguish_range_end = std::atoi(range_val.c_str());
+        i++;
+      }
+      break;
+    }
+    default: break;
+    }
+  }
+  
+  if (verbose_flag) {
+    opt.verbose = true;
+  }
+  if (pipe_flag) {
+    opt.stream_out = true;
+  }
+  
+  if (distinguish_all_flag) {
+    opt.distinguish_union = true;
+  }
+  if (distinguish_all_but_one_flag) {
+    opt.distinguish_all_but_one_color = true;
+  }
+  
+  for (int i = optind; i < argc; i++) {
+    opt.transfasta.push_back(argv[i]);
+  }
+}
+
+
 void ParseOptionsRefine(int argc, char **argv, ProgramOptions& opt) {
   int verbose_flag = 0;
   int pipe_flag = 0;
@@ -360,11 +453,87 @@ bool CheckOptionsDistinguish(ProgramOptions& opt) {
   return ret;
 }
 
+
+
+bool CheckOptionsRefineUnitigs(ProgramOptions& opt) {
+  
+  bool ret = true;
+  
+  if (opt.threads <= 0) {
+    cerr << "Error: invalid number of threads " << opt.threads << endl;
+    ret = false;
+  } else {
+    unsigned int n = std::thread::hardware_concurrency();
+    if (n != 0 && n < opt.threads) {
+      cerr << "Warning: you asked for " << opt.threads
+           << ", but only " << n << " cores on the machine" << endl;
+      opt.threads = n;
+    }
+  }
+
+  if (opt.k <= 1 || opt.k >= MAX_KMER_SIZE) {
+    cerr << "Error: invalid k-mer length " << opt.k << ", minimum is 3 and maximum is " << (MAX_KMER_SIZE -1) << endl;
+    ret = false;
+  }
+
+  if (opt.k % 2 == 0) {
+    cerr << "Error: k needs to be an odd number" << endl;
+    ret = false;
+  }
+
+  if (opt.transfasta.empty()) {
+    cerr << "Error: no FASTA files specified" << endl;
+    ret = false;
+  } else {
+    for (auto& fasta : opt.transfasta) {
+      // we want to generate the index, check k, index and transfasta
+      struct stat stFileInfo;
+      auto intStat = stat(fasta.c_str(), &stFileInfo);
+      if (intStat != 0) {
+        cerr << "Error: FASTA file not found " << fasta << endl;
+        ret = false;
+      }
+    }
+  }
+  
+  if (opt.distinguish_output_fasta.empty() && !opt.stream_out) {
+    cerr << "Error: need to specify output FASTA file name or use --pipe" << endl;
+    ret = false;
+  } else if (!opt.distinguish_output_fasta.empty() && opt.stream_out) {
+    cerr << "Error: cannot supply both output FASTA file name and --pipe" << endl;
+    ret = false;
+  }
+  if (opt.stream_out && opt.verbose) {
+    cerr << "Error: cannot supply both --verbose and --pipe" << endl;
+    ret = false;
+  }
+  
+  if (opt.distinguish_union && opt.distinguish_all_but_one_color) {
+    cerr << "Error: Cannot use multiple distinguish options" << endl;
+    ret = false;
+  }
+  
+  if (opt.g != 0) {
+    if (opt.g <= 2 || opt.g > opt.k - 2) {
+      cerr << "Error: invalid minimizer size " << opt.g << ", minimum is 3 and maximum is k - 2" << endl;
+      ret = false;
+    }
+  }
+  
+  if (opt.distinguish_range_end == 0) {
+    opt.distinguish_range_end = opt.distinguish_range_begin;
+  }
+  if (opt.distinguish_range_end < opt.distinguish_range_begin) {
+    cerr << "Error: invalid range supplied" << endl;
+    ret = false;
+  }
+  
+  return ret;
+}
+
 bool CheckOptionsRefine(ProgramOptions& opt) {
 
   bool ret = true;
-  
-  
   
   if (opt.k <= 1 || opt.k >= MAX_KMER_SIZE) {
     cerr << "Error: invalid k-mer length " << opt.k << ", minimum is 3 and maximum is " << (MAX_KMER_SIZE -1) << endl;
@@ -481,6 +650,25 @@ void usageDistinguish() {
 
 }
 
+void usageRefineUnitigs() {
+  cout << "kure " << KURE_VERSION << endl
+       << "Reconstruct contigs in FASTA file, outputting extended contig sequences" << endl << endl
+       << "Usage: kure reconstruct [arguments] FASTA-files" << endl << endl
+       << "Required argument (choose one of the following):" << endl
+       << "-p, --pipe                 Direct output to standard output" << endl
+       << "-o, --output=STRING        Filename for the output FASTA" << endl << endl
+       << "Optional argument:" << endl
+       << "-k, --kmer-size=INT         k-mer (odd) length (default: 31, max value: " << (MAX_KMER_SIZE-1) << ")" << endl
+       << "-r, --range=INT-INT         Set the range of of length of output sequences (format: begin-end)" << endl
+       << "                            Can specify multiple numbers (one for each input file)" << endl
+       << "    --all                   Set the mode to be extracting sequences found in any input" << endl
+       << "    --all-but-one           Set the mode to be extracting all sequences except those found across all inputs" << endl
+       << "-t, --threads=INT           Number of threads to use (default: 1)" << endl
+       << "-m, --min-size=INT          Length of minimizers (default: automatically chosen)" << endl
+       << endl;
+  
+}
+
 void usageRefine() {
   cout << "kure " << KURE_VERSION << endl
        << "Refines contigs in FASTA file, outputting the ones that meet certain criteria" << endl << endl
@@ -561,6 +749,22 @@ int main(int argc, char *argv[]) {
           }
           out_map_f.close();
         }
+      }
+    } else if (cmd == "reconstruct") {
+      if (argc==2) {
+        usageRefineUnitigs();
+        return 0;
+      }
+      ParseOptionsRefineUnitigs(argc-1,argv+1,opt);
+      if (!CheckOptionsRefineUnitigs(opt)) {
+        usageRefineUnitigs();
+        exit(1);
+      } else {
+        Kmer::set_k(opt.k);
+        //KmerIndex index(opt);
+        std::ofstream out;
+        //out.open(opt.distinguish_output_fasta, std::ios::out | std::ios::binary);
+        //index.BuildDistinguishingGraph(opt, out);
       }
     } else if (cmd == "refine") {
       if (argc==2) {
