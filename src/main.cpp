46832414c8784c90d9b4d20f098ce6e1ea0edb86
mboffelli <mayg.bof@gmail.com>
2023-08-15 17:43:34 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -69,7 +69,7 @@ void ParseOptionsDistinguish(int argc, char **argv, ProgramOptions& opt) {
     {"verbose", no_argument, &verbose_flag, 1},
     {"all", no_argument, &distinguish_all_flag, 1},
     {"all-but-one", no_argument, &distinguish_all_but_one_flag, 1},
-    {"all-but-N-colors", no_argument, &distinguish_all_but_N_flag, 1, // for all-but-N
+    {"all-but-N-colors", no_argument, &distinguish_all_but_N_flag, 1}, // for all-but-N
     {"colors-to-retain", required_argument, 0, 'R'},  // for all-but-N
     // short args
     {"output", required_argument, 0, 'o'},
