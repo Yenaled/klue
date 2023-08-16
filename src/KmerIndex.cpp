6bfea38b336abdabd26d444946df03dffdb37886
mboffelli <mayg.bof@gmail.com>
2023-08-16 11:59:10 -0700

    hard coded for testing

--- src/KmerIndex.cpp
+++ src/KmerIndex.cpp
@@ -339,7 +339,7 @@ void KmerIndex::BuildDistinguishingGraph(const ProgramOptions& opt, const std::v
   // TESTING only, add opt.min_contig_num to ProgramOptins
   int min_contig_num = 2;  // specify contig extraction
   int max_contig_num = 8; // specify contig extraction
-  std::unordered_set<int> colors_to_retain = { 2 , 4 }; // TESTING only, add colors_to_retain to ProgramOptions
+  std::unordered_set<int> colors_to_retain = { 2 , 4 , 7}; // TESTING only, add colors_to_retain to ProgramOptions
   // TODO: Reconstruct below
   for (const auto& unitig : ccdbg) {
     const UnitigColors* uc = unitig.getData()->getUnitigColors(unitig);
@@ -375,8 +375,8 @@ void KmerIndex::BuildDistinguishingGraph(const ProgramOptions& opt, const std::v
 
                 // TESTING only, add opt.colorsToRetain to ProgramOptions
                 // retain color based on specified set of colors
-                if (colors_to_retain.count(color) == 0){
-                  continue;
+                if (colors_to_retain.count(color) == 0){ // if the current color is NOT in colors_to_retain
+                  continue;                              // skip current iteration of loop (i.e. leave out of graph?)
                 }
 
                 // add to Program options
@@ -385,42 +385,51 @@ void KmerIndex::BuildDistinguishingGraph(const ProgramOptions& opt, const std::v
                   continue;
                 }
                 ***/
-              }
-              if (!opt.distinguish_all_but_one_color && !opt.distinguish_union) {
-                int i_ = 0;
-                for (const auto& k_elem : k_map) {
-                  int j_ = 0;
-                  for (const auto& k_elem2 : k_map) {
-                    if (j_ > i_ && k_elem.first != k_elem2.first) {
-                      std::set<int> intersect;
-                      std::set<int> set_result;
-                      std::set_intersection(k_elem.second.begin(), k_elem.second.end(), k_elem2.second.begin(), k_elem2.second.end(), std::inserter(intersect, intersect.begin())); //if (k_elem2.second.count(k_elem1.second)) // check if set intersection with k_elem2
-                      std::set_union(positions_to_remove.begin(), positions_to_remove.end(), intersect.begin(), intersect.end(), std::inserter(set_result,set_result.begin()));
-                      positions_to_remove = std::move(set_result);
+                if (!opt.distinguish_all_but_one_color && !opt.distinguish_union) {
+                    int i_ = 0;
+                    for (const auto& k_elem : k_map) {
+                        int j_ = 0;
+                        for (const auto& k_elem2 : k_map) {
+                            if (j_ > i_ && k_elem.first != k_elem2.first) {
+                                std::set<int> intersect;
+                                std::set<int> set_result;
+                                std::set_intersection(k_elem.second.begin(), k_elem.second.end(), k_elem2.second.begin(), k_elem2.second.end(), std::inserter(intersect, intersect.begin())); //if (k_elem2.second.count(k_elem1.second)) // check if set intersection with k_elem2
+                                std::set_union(positions_to_remove.begin(), positions_to_remove.end(), intersect.begin(), intersect.end(), std::inserter(set_result, set_result.begin()));
+                                positions_to_remove = std::move(set_result);
+                            }
+                            j_++;
+                        }
+                        i_++;
                     }
-                    j_++;
-                  }
-                  i_++;
                 }
-              } else if (!opt.distinguish_union) {
-                int i_ = 0;
-                if (k_map.size() == tmp_files.size()) {
-                  for (const auto& k_elem : k_map) {
-                    i_++;
-                    if (positions_to_remove.size() == 0) {
-                      positions_to_remove = k_elem.second;
-                    } else {
-                      std::set<int> set_result;
-                      std::set_intersection(positions_to_remove.begin(), positions_to_remove.end(), k_elem.second.begin(), k_elem.second.end(), std::inserter(set_result,set_result.begin()));
-                      positions_to_remove = std::move(set_result);
+                else if (!opt.distinguish_union) {
+                    int i_ = 0;
+                    if (k_map.size() == tmp_files.size()) {
+                        for (const auto& k_elem : k_map) {
+                            i_++;
+                            if (positions_to_remove.size() == 0) {
+                                positions_to_remove = k_elem.second;
+                            }
+                            else {
+                                std::set<int> set_result;
+                                std::set_intersection(positions_to_remove.begin(), positions_to_remove.end(), k_elem.second.begin(), k_elem.second.end(), std::inserter(set_result, set_result.begin()));
+                                positions_to_remove = std::move(set_result);
+                            }
+                        }
                     }
-                  }
                 }
               }
+              
               for (const auto& k_elem : k_map) {
                 int curr_pos = -1;
                 std::string colored_contig = "";
                 auto color = k_elem.first;
+
+                // Check if the color should be retained
+                if (colors_to_retain.count(color) == 0) {
+                    continue; // Skip positions not corresponding to retained colors
+                }
+
                 //std::string contig_metadata = " :" + unitig.dist + "," + unitig.len + "," + unitig.size + "," + unitig.strand;
                 for (const auto &pos : k_elem.second) {
                   if (!positions_to_remove.count(pos)) {
